ae983a6e796b0da7316e9872ffd90719fb8796d6
Aguzr10 <a.guzmanr2@uniandes.edu.co>
2025-02-15 22:11:38 -0500

    corrección pre-mian

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ from parser import parse
 
 def leer_archivo(nombre_archivo):
     with open(nombre_archivo, "r") as file:
-        return file.read()
+        return file.readlines()  # Devuelve una lista de líneas
 
 if __name__ == "__main__":
-    codigo = leer_archivo("codigo.txt")
+    lineas = leer_archivo("codigo.txt")  
+    codigo = "".join(lineas)  # Convierte la lista en una sola cadena
+
     tokens = tokenize(codigo)
     resultado = parse(tokens)
     print(resultado)  # "Sí" o "No"
